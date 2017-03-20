c5ee9f271d7b4cbdc324113749ccd4de33fa5041
Mike Umansky <longjustn@gmail.com>
2017-03-20 20:38:15 +0200

    Added Node layer

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
 var express = require('express');
 var path = require('path');
+var http = require('http');
+var config = require('./config');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var errorHandler = require('errorhandler');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+if (app.get('env') === 'development') {
+    app.use(logger('dev'));
+} else {
+    app.use(logger('default'));
+}
+if ('development' === app.get('env')) {
+    app.use(errorHandler());
+}
 
-app.use('/', index);
-app.use('/users', users);
+http.createServer(app).listen(config.get('port'), function(){
+    console.log('Express server listening on port ' + config.get('port'));
+});
 
-// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+    if (req.url == '/') {
+        res.end("Hello world");
+    } else {
+        next();
+    }
+});
+
+app.get('/', function (req, res) {
+    res.render('GET request to the homepage');
+});
+
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//error handler
+app.use(function(err, req, res) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    res.status(err.status || 500);
+    res.render('error');
+});*/
+/*
 
-module.exports = app;
+
+module.exports = app;*/
