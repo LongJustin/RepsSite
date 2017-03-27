8abc51ef9894fdc972cf28cbd836ab3420a4bf4e
Mike Umansky <longjustn@gmail.com>
2017-03-27 21:01:44 +0300

    Adding mailer

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var config = require('./config');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -19,14 +21,89 @@ if (app.get('env') === 'development') {
 if ('development' === app.get('env')) {
     app.use(errorHandler());
 }
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post('/send', function(req, res) {
+    if (req.body.mail === '' || req.body.subject === '') {
+        res.send("Email & Subject should not be blank");
+        return false;
+    }
+
+    var smtpTransport = nodemailer.createTransport({
+        service: 'gmail',
+        secure: false,
+        port: 25,
+        auth: {
+            user: 'longjustn@gmail.com',
+            pass: config.get('password')
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    var MailOptions = {
+        from: req.body.mail,
+        to: 'mike@reps.ai',
+        subject: 'Reps AI contact',
+        text: req.body.message
+    };
+    smtpTransport.sendMail(MailOptions, function(error, res) {
+        if (error) {
+            res.send("Email could not be sent due to error:" + error);
+        } else {
+            res.send("Email has been sent");
+        }
+
+    });
+});
+
+
+// Nodemailer try1
+/*var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    port: 25,
+    auth: {
+        user: 'longjustn@gmail.com',
+        pass: config.get('password')
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+var MailOptions = {
+    from: '"Michael Umansky" <longjustn@gmail.com',
+    to: 'longjustn@gmail.com',
+    subject: 'Reps AI contact',
+    text: 'Shalom'
+};
+
+transporter.sendMail(MailOptions, function(error, info) {
+    if (error) return console.log(error);
+    console.log("The message was sent");
+    console.log(info);
+});*/
 
 http.createServer(app).listen(config.get('port'), function(){
     console.log('Express server listening on port ' + config.get('port'));
 });
 
 /*app.use(function(req, res, next) {
-    if (req.url == '/') {
-        res.end("Hello world");
+    if (req.url === '/') {
+        res.render("index");
+    } else {
+        next();
+    }
+});
+
+app.use(function(req, res, next) {
+    if (req.url === '/terms') {
+        res.render("terms");
     } else {
         next();
     }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,41 @@
-$(document).ready(function(){
-        $(".link").click(function (){
-            $("body,html").animate({
-                scrollTop:0
-            }, 800);
-            return false;
+ $(document).ready(function(){
+     $("body").css("display", "none").fadeIn(500);
+
+     $("a.transition").click(function(event){
+         event.preventDefault();
+         linkLocation = this.href;
+         $("body").fadeOut(500, redirectPage);
+     });
+
+     function redirectPage() {
+         window.location = linkLocation;
+     }
+
+    $("#trial").click(function (){
+        $('html, body').animate({scrollTop:$(document).height()}, 800);
+        return false;
+    });
+
+    $(".header").click(function (){
+        $('html, body').animate({scrollTop:0}, 800);
+        return false;
+    });
+
+    var fullUrl = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port: '');
+    $("#Send").click(function(){
+        var formData = $("#emailForm").serialize();
+        $.ajax ({
+            url: fullUrl + '/send',
+            type: 'POST',
+            data: formData,
+            success: function(result) {
+                $("#msg").empty().text(result);
+            },
+            error: function(err) {
+                $("#msg").empty().text("There is error " + err.status + ", error message: " + err.statusText);
+            },
+            dataType: "html",
+            timeout: 60000
         });
-});
+    });
+ });
